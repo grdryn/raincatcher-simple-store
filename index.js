81be422763f650bfaabee0f8e1188108cc4d13b9
Paolo Bueno <paolohaji@gmail.com>
2017-01-03 11:57:59 -0200

    Add logging to confirm which version of the store is selected

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 if (process.env.WFM_USE_MEMORY_STORE) {
+  console.log('Using In-memory Store');
   module.exports = require('./lib/array-store');
 } else {
+  console.log('Using In-memory Store');
   module.exports = require('./lib/fh-db-store');
 }
