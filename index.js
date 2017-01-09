3baae0f1f3d1b7b7426a66eb7b2de0c2de25031a
Paolo Bueno <paolohaji@gmail.com>
2017-01-09 10:42:57 -0200

    Fix logging line for fh-db

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ if (process.env.WFM_USE_MEMORY_STORE) {
   console.log('Using In-memory Store');
   module.exports = require('./lib/array-store');
 } else {
-  console.log('Using In-memory Store');
+  console.log('Using $fh.db Store');
   module.exports = require('./lib/fh-db-store');
 }
