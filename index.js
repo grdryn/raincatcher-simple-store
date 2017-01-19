12729cfb11d46be0baae493a4ae9261a36db7cfe
Paolo Bueno <paolohaji@gmail.com>
2017-01-19 14:13:11 -0200

    Decide implementation based on config instead of only env var

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-if (process.env.WFM_USE_MEMORY_STORE === "true") {
-  console.log('Using In-memory Store');
-  module.exports = require('./lib/array-store');
-} else {
-  console.log('Using $fh.db Store');
-  module.exports = require('./lib/fh-db-store');
-}
+var _ = require('lodash');
+module.exports = function selectStore(config) {
+  config = _.defaults(config, {
+    persistent: process.env.WFM_USE_MEMORY_STORE !== "true"
+  });
+  if (config.persistent) {
+    console.log('Using $fh.db Store');
+    return require('./lib/fh-db-store');
+  } else {
+    console.log('Using In-memory Store');
+    return require('./lib/array-store');
+  }
+};
